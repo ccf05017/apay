dee9c94a1e4a28558312227381f09efe1a2f9574
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-28 13:24:56 +0900

    deploy

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker stop haepaystorefront
 docker build -t haepaystorefront .
-docker run -it -p 8888:3000 -d --rm --name haepayfront haepaystorefront
+docker run -it -p 3000:3000 -d --rm --name haepaystorefront haepaystorefront
